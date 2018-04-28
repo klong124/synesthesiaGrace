acc91d99b66f3887660689c2c04b974abe08f20b
Priya <banspri0@gmail.com>
2018-04-28 18:18:22 -0400

    Working on changing pages when winning or losing

--- client/components/Level.js
+++ client/components/Level.js
@@ -1,9 +1,22 @@
 import React from 'react'
-import PropTypes from 'prop-types'
 import {connect} from 'react-redux'
 import _ from 'lodash'
 import {noteRec, updateNotes} from '../store'
 import Tone from 'tone'
+const synth = new Tone.Synth().toMaster()
+import Won from './Won'
+import Lost from './Lost'
+let won, lost
+const colorToSound =
+  {
+    "R": "G4",
+    "O": "A4",
+    "Y": "B#4",
+    "G": "C5",
+    "B": "D5",
+    "I": "E5",
+    "V": "F5"
+  }
 
 /**
  * COMPONENT
@@ -14,18 +27,21 @@ class Level extends React.Component
   {
     this.props.loadLevel()
     console.log("notes upon mount are", this.props.notes)
+    won, lost = false //Player hasn't won or lost at mount
     addEventListener("keypress", e =>
     {
       e.preventDefault()
       const {notes} = this.props;
-      console.log("Note has been pressed")
+
+      //Play note whenever key is pressed
+      synth.triggerAttackRelease(colorToSound[e.key.toUpperCase()], '8n')
 
       if (e.key.toUpperCase() !== notes[0])
-        console.log("You lost :(")
+        lost = true
       else
       {
-        if (notes.length === 1)
-          console.log("You won!")
+        if (notes.length === 0)
+          won = true
         else
           this.props.update(notes.slice(1))
       }
@@ -45,20 +61,41 @@ class Level extends React.Component
         "I": "indigo",
         "V": "violet"
       }
-    return ( //Rerenders every time key is pressed, but now it's not a bug, it's a feature ;)
-      <div className = "notes">
-        {
-          _.uniq(notes).sort(function(a, b){return 0.5 - Math.random()}).map(note =>
+    if (!won && !lost)
+    {
+      console.log("Goes in here")
+      return ( //Rerenders every time key is pressed, but now it's not a bug, it's a feature ;)
+        <div className="notes">
           {
-            return (
-              <svg className = {"note " + keyToClassName[note]} key = {note}>
-                <rect width = "100" height = "100"/>
-              </svg>
-            )
-          })
-        }
-      </div>
-    )
+            _.uniq(notes).sort(function (a, b)
+            {
+              return 0.5 - Math.random()
+            }).map(note =>
+            {
+              return (
+                <svg className={"note " + keyToClassName[note]} key={note}>
+                  <rect width="100" height="100"/>
+                </svg>
+              )
+            })
+          }
+        </div>
+      )
+    }
+    if (won)
+    {
+      console.log("WON")
+      return (
+        <Won/>
+      )
+    }
+    if (lost)
+    {
+      console.log("LOST")
+      return (
+        <Lost/>
+      )
+    }
   }
 
 }
@@ -73,21 +110,10 @@ const mapState = (state) => {
 }
 
 const mapDispatch = (dispatch, ownProps) => {
-  const colorToSound =
-    {
-      "R": "G4",
-      "O": "A4",
-      "Y": "B#4",
-      "G": "C5",
-      "B": "D5",
-      "I": "E5",
-      "V": "F5"
-    }
   return {
     loadLevel () {
       dispatch(noteRec(ownProps.match.params.levelId)).then((action) =>
       {
-        const synth = new Tone.Synth().toMaster()
         action.notes.forEach((note, time) =>
         {
           synth.triggerAttackRelease(colorToSound[note], '8n', time)
@@ -101,10 +127,3 @@ const mapDispatch = (dispatch, ownProps) => {
 }
 
 export default connect(mapState, mapDispatch)(Level)
-//
-// /**
-//  * PROP TYPES
-//  */
-// Home.propTypes = {
-//   notes: PropTypes.string
-// }
---
+++ client/components/Lost.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import {connect} from 'react-redux'
+
+/**
+ * COMPONENT
+ */
+class Level extends React.Component
+{
+  render()
+  {
+    return ( //Rerenders every time key is pressed, but now it's not a bug, it's a feature ;)
+      <div>
+        You're a loser
+      </div>
+    )
+  }
+
+}
+
+/**
+ * CONTAINER
+ */
+const mapState = (state) => {
+  return {
+    notes: state.notes
+  }
+}
+
+export default connect(mapState)(Level)
